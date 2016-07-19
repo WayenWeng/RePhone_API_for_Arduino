3a26c2bd79c2ca8a6567bbd8a79f9c9b5e63593d
WayneWeng <jinyuan.weng@seeed.cc>
2016-07-19 13:40:58 +0800

    Update
    Fix the bug of NFC example reaad.

--- examples/nfc_test/nfc_test.ino
+++ examples/nfc_test/nfc_test.ino
@@ -7,6 +7,20 @@ void setup()
 {
 	Serial.begin(115200);
 	Serial.print("NFC test.\r\n");
+    
+    while(1)
+    {
+        if(LNFC.check_on_line())
+        {
+            Serial.println("On-line!");
+            break;
+        }
+        else Serial.println("Off-line!");
+        delay(1000);
+    }
+    
+    LNFC.adapter_init();
+    delay(1000);
 }
     
 void loop()
@@ -14,20 +28,14 @@ void loop()
 	unsigned char *str = 0;
 	char buffer[50] = {0,};
 	unsigned char i;
-    
-	if(LNFC.check_on_line())
-	{
-		LNFC.adapter_init();
-		delay(50);
 
-		str = LNFC.adapter_get_uid();
-		sprintf(buffer, "NFC uid is 0x%x 0x%x 0x%x 0x%x\r\n", str[2],str[3],str[4],str[5]);
-		Serial.print(buffer);
+    str = LNFC.adapter_get_uid();
+    sprintf(buffer, "NFC uid is 0x%x 0x%x 0x%x 0x%x\r\n", str[2],str[3],str[4],str[5]);
+    Serial.print(buffer);
 
-		str = LNFC.adapter_read();
-		sprintf(buffer, "NFC read is %s \r\n", str + 2);
-		Serial.print(buffer);
-	}
+    str = LNFC.adapter_read();
+    sprintf(buffer, "NFC read is %s \r\n", str + 2);
+    Serial.print(buffer);
 	
 	i = 20;
 	while(i--)
